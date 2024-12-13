a42babd25ca3b3ab9f4c573927f2c24c3edb0c88
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2024-12-13 21:09:19 +0400

    add dinamic profile pictures

--- src/components/UI/ProfilePicture/ProfilePicture.jsx
+++ src/components/UI/ProfilePicture/ProfilePicture.jsx
@@ -1,10 +1,23 @@
 import styles from "./styles.module.css";
 import { PROFILE_ICONS } from "../../../constants/PROFILE_ICONS";
+import { useSelector } from "react-redux";
 
 const ProfilePicture = () => {
+  const gender = useSelector((state) => state.account.gender);
+
   return (
     <div className={styles.profile}>
-      <img src={PROFILE_ICONS.man2} alt="Фото профиля" className={styles.img} />
+      <img
+        src={
+          PROFILE_ICONS[
+            gender === "мужской"
+              ? `man${Math.ceil(Math.random() * 6)}`
+              : `woman${Math.ceil(Math.random() * 6)}`
+          ]
+        }
+        alt="Фото профиля"
+        className={styles.img}
+      />
     </div>
   );
 };
